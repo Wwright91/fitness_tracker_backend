4e08ac49416bb752fc1dd06d7b48f08dac3752c8
Wwright91 <wisdomwright-martin@pursuit.org>
2024-06-29 16:19:57 -0400

    GET /workouts
    1. Added array of workout data
    2. In app.js added route `/workouts`
    3. In workoutsController.js implemented route to get all workouts

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+// DEPENDENCIES
 const express = require("express");
 
+// CONFIGURATION
 const app = express();
+
+// CONTROLLERS
+const workoutsController = require("./controllers/workoutsController")
 
 // HEALTH CHECK ROUTE
 app.get("/", (req, res) => {
   res.send("Welcome to my fitness app!")
 })
+
+// WHEN RECEIVING A REQUEST OR RESPONSE ON THE ROUTE `/workouts` WE WANT OUR APP TO USE THE `workoutsController`
+app.use("/workouts", workoutsController)
 
 module.exports = app;
