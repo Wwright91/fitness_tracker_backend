bd25c6dcc1bb7c7c2676063b42e7a2ddf2aa1d40
Wwright91 <wisdomwright-martin@pursuit.org>
2024-06-29 16:41:17 -0400

    GET /workouts/:id
    1. In workoutsController.js implemented route to get a workout by id

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -7,4 +7,15 @@ router.get("/", (req, res) => {
   res.status(200).send(workoutArray);
 });
 
+// GET A SINGLE WORKOUT
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const workout = workoutArray.find(el => el.id === +id)
+  if (workout) {
+    res.status(200).send(workout)
+  } else {
+    res.status(404).json({error: `Workout with id: ${id} not found!`})
+  }
+})
+
 module.exports = router;
