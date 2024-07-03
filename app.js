0335d3e7645570a5582abab419990bd2e6d1868f
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-02 21:03:50 -0400

    Installed cors and implemented usage in app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // DEPENDENCIES
 const express = require("express");
+const cors = require("cors")
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
 app.use(express.json())
+app.use(cors())
 
 // CONTROLLERS
 const workoutsController = require("./controllers/workoutsController")
