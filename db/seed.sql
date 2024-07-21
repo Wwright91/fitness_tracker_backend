5a78270360988c48005bcb416f58d84a08c5c0b6
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-21 16:50:00 -0400

    Set up database
    1. Created dbConfig
    2. Created schema
    3. Created seed
    4. Installed pg-promise

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+\c fitness_tracker;
+
+INSERT INTO workouts (type, durationInMinutes, caloriesBurned, date) VALUES
+('Running', 30, 300, '2024-06-20'),
+('Cycling', 45, 400, '2024-06-21'),
+('Swimming', 60, 500, '2024-06-22'),
+('Yoga', 40, 200, '2024-06-23'),
+('Weight Lifting', 50, 350, '2024-06-24'),
+('Elliptical Training', 30, 250, '2024-06-25'),
+('Pilates', 30, 150, '2024-06-26'),
+('Jump Rope', 15, 200, '2024-06-26'),
+('Rowing', 35, 300, '2024-06-27'),
+('Dance', 60, 400, '2024-06-28');
